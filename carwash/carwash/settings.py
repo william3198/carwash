5d45478917b9bcfc8c6e5474622f31f0f2b5d0cf
WQC <william_3198@hotmail.com>
2026-08-20 17:32:54 -0500

    Configura archivos estaticos en Render

--- carwash/carwash/settings.py
+++ carwash/carwash/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,5 +117,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MAILERS = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
+    },
+}
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
